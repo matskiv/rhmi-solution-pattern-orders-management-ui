f0640f4314173cbfafde15c289be8fb34c8c2483
Evan Shortiss <evanshortiss@gmail.com>
2019-12-06 10:45:27 -0800

    fix: target integreatly repo on quay.io

--- scripts/image.push.sh
+++ scripts/image.push.sh
@@ -1,4 +1,4 @@
-QUAY_USER=${QUAY_USER:-evanshortiss}
+QUAY_USER=${QUAY_USER:-integreatly}
 TAG=${TAG:-latest}
 docker tag rhmi-lab-order-management-ui quay.io/$QUAY_USER/rhmi-lab-order-management-ui:$TAG
 docker push quay.io/$QUAY_USER/rhmi-lab-order-management-ui:$TAG
