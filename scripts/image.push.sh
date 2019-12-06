a42eec442c0ca6b97755a7a248f453407b63f360
Evan Shortiss <evanshortiss@gmail.com>
2019-12-06 10:43:21 -0800

    chore: update scripts, package.json, and template

--- scripts/image.push.sh
+++ scripts/image.push.sh
@@ -1,3 +1,4 @@
 QUAY_USER=${QUAY_USER:-evanshortiss}
-docker tag rhmi-lab-order-management-ui quay.io/$QUAY_USER/rhmi-lab-order-management-ui:latest
-docker push quay.io/$QUAY_USER/rhmi-lab-order-management-ui:latest
+TAG=${TAG:-latest}
+docker tag rhmi-lab-order-management-ui quay.io/$QUAY_USER/rhmi-lab-order-management-ui:$TAG
+docker push quay.io/$QUAY_USER/rhmi-lab-order-management-ui:$TAG
